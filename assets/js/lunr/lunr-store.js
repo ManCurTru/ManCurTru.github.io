4c690fa7e31a362d46a7ba369b03175de4f31f7f
ManCurTru <alu0100045130@ull.edu.es>
2021-12-02 18:22:54 +0000

    new-deploy

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,37 @@
+var store = [{
+        "title": "El first post",
+        "excerpt":"Mañana trabajo y la proxima semana estoy de tarde asi que no puedo venir a clase   asi que me voy a acostar a las 21:00 como las gallinas para mañana no estar reventado  ","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/aet/",
+        "teaser": null
+      },{
+        "title": "Me gustan las peliculas de los 80's",
+        "excerpt":"Segundo post tal y cual toma tomatoes Esto es bastante para que sirve esto del lost engorroso \\[f(x) = \\int_{-\\infty}^\\infty \\hat f(\\alpha)\\,e^{2 \\pi i \\xi x} \\,d\\xi\\] \\[x = \\frac {-b \\pm \\sqrt {b^2 - 4ac}}{2a}\\] No se en que dia vivo, que es lo que pasa en la vida...","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/PostII/",
+        "teaser": null
+      },{
+        "title": "Clase",
+        "excerpt":"Tercer Post   Titulo: Testeando Tablas —                                Nombre                Apellido                Edad                Lugar                               Manuel            Curbelo            42            La Juncia                            Montse            Garcia            45            Palo Blanco                            Ainara            Alvarez            16            Chimiche                            Carmen Nieves            Trujillo            59            EL Palmar           ","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/clase/",
+        "teaser": null
+      },{
+        "title": "Didactica",
+        "excerpt":"La didáctica (del adj. didáctico, del griego διδακτικός [didaktikós]).1​2​ Entre los pensadores griegos, aparecieron los primeros acercamientos etimológicos a la Didáctica, con alusiones, imprecisas y generales, pero útiles, a la enseñanza y al aprendizaje. La palabra didáctica tiene su origen en el término diaktiqué, asociado al arte de enseñar. “En...","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/didactica/",
+        "teaser": null
+      },{
+        "title": "Aprendizaje explicito",
+        "excerpt":"El aprendizaje explícito se caracteriza porque el aprendiz tiene intención de aprender y es consciente de qué aprende. Por ejemplo, este tipo de aprendizaje nos permite adquirir información sobre personas, lugares y objetos. Es por eso que esta forma de aprender exige de atención sostenida y selectiva del área más...","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/Aprendizaje-explicito/",
+        "teaser": null
+      },{
+        "title": "Aprendizaje implicito",
+        "excerpt":"El aprendizaje implícito hace referencia a un tipo de aprendizaje que se constituye en un aprendizaje generalmente no-intencional y donde el aprendiz no es consciente sobre qué se aprende. El resultado de este aprendizaje es la ejecución automática de una conducta motora. Lo cierto es que muchas de las cosas...","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/Aprendizaje-implicito/",
+        "teaser": null
+      }]
